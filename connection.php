9c4365e51c55ce96c0a88d2411d50ea2f39fdbbf
cungdanhbotay <91724418+cungdanhbotay@users.noreply.github.com>
2022-05-28 10:53:08 +0700

    upload

---
+++ connection.php
@@ -0,0 +1,3 @@
+<?php
+    $conn = pg_connect('postgres://idqxqhonvyarmf:0704708e985b391ef7652a1fdcfc9d99eed07e8102ccbf9146dd5baa91a97379@ec2-3-228-235-79.compute-1.amazonaws.com:5432/d3ddd35rl7mbl5') or die("Can not connect database".pg_error());
+?>
